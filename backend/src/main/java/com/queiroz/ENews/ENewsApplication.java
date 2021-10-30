a6f690e2b1ad8cbd9d9b3c3322ca30636458adff
Matheus Queiroz <scririft@gmail.com>
2021-10-30 20:19:06 -0300

    Project created

---
+++ backend/src/main/java/com/queiroz/ENews/ENewsApplication.java
@@ -0,0 +1,13 @@
+package com.queiroz.ENews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENewsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/queiroz/ENews/ENewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queiroz.ENews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
